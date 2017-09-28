01f72b3d5f55f3300ab44564e1fede007e49551f
Rei Lin <temisrei@gmail.com>
2017-09-28 11:47:35 +0800

    add Form Validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   subscriptions = ['Basic', 'Advanced', 'Pro'];
   selectedScubscription = 'Advanced';
+
+  @ViewChild('signupForm') sgnForm: NgForm;
+  onSubmit() {
+    console.log(this.sgnForm.value);
+  }
 }
