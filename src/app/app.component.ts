51b3f0ec71a7d004ea4ec58a140a1ba83bf44651
Rei Lin <temisrei@gmail.com>
2017-09-28 11:31:43 +0800

    create basic template-driven form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  subscriptions = ['Basic', 'Advanced', 'Pro'];
+  selectedScubscription = 'Advanced';
 }
